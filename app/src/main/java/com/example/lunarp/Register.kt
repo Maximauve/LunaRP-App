c34dfdfa87196973d89ef8b81c149c925046b500
Hyuga974 <tamashiyami@gmail.com>
2023-03-04 23:13:03 +0100

    Update Register: only one error message

--- app/src/main/java/com/example/lunarp/Register.kt
+++ app/src/main/java/com/example/lunarp/Register.kt
@@ -21,6 +21,9 @@ import kotlin.reflect.typeOf
 class Register : AppCompatActivity() {
     var errorMessage = ""
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        val main = Intent(this, MainActivity::class.java)
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
@@ -34,29 +37,62 @@ class Register : AppCompatActivity() {
 
         // set on-click listener
         btn_submit_.setOnClickListener {
+            errorMessage = ""
             val username = user_name_.text.toString();
             val userMail = user_mail_.text.toString();
             val password = password_.text.toString();
             val confirmPassword = confirmPassword_.text.toString();
 
-            if (user_name_.text.isNotEmpty() && password_.text.isNotEmpty()){
-                println(password)
-                println(confirmPassword)
-                println(password == confirmPassword)
-                if (password == confirmPassword){
-                    signUp(username,userMail, password)
-                } else {
-                    println("HEEEEEEEEEERE")
-                    Toast.makeText(this@Register, "Ce ne sont pas les même mot de passe", Toast.LENGTH_LONG).show()
-                }
-            } else {
+            println(errorMessage.isEmpty())
+            if (user_name_.text.isEmpty() && password_.text.isEmpty()){
                 Toast.makeText(this@Register, "Remplissez tous les champs", Toast.LENGTH_LONG).show()
-            }
-
-            if (SessionManager.isLogin()){
-                finish()
             } else {
-                Toast.makeText(this@Register, errorMessage, Toast.LENGTH_LONG).show()
+                if (password != confirmPassword){
+                    Toast.makeText(this@Register, "Ce ne sont pas les même mot de passe", Toast.LENGTH_LONG).show()
+                } else {
+                    ///
+
+                    println(":::::::::: SIGN UP ::::::::::")
+
+                    var retrofit = RequestUtils.retrofitBase.create(UserInterface::class.java)
+                    val user = UserRegisterClassItem(email = userMail, username = username, password = password, role = "User")
+                    var retrofitData = retrofit.register(user)
+
+                    retrofitData.enqueue(object: Callback<UserClassItem>{
+                        override fun onFailure(call: Call<UserClassItem>, t: Throwable) {
+                            Log.d("RegisterActivity", "onfailure: "+ t.message )
+                        }
+
+                        override fun onResponse(call: Call<UserClassItem>, response: Response<UserClassItem>) {
+                            val errorStr = response.errorBody()?.string()
+                            if (response.isSuccessful) {
+                                SessionManager.logIn(userMail, password);
+                                startActivity(main)
+                                finish()
+                            }
+                            when (response.code()){
+                                400 -> {
+                                    if (errorStr != null) {
+                                        if (errorStr.contains("email must be an email")) {
+                                            Toast.makeText(this@Register, "email doit être un email", Toast.LENGTH_LONG).show()
+                                        } else if (errorStr.contains("password must be longer than or equal to 8 characters")){
+                                            Toast.makeText(this@Register, "Mot de passe trop court: minimum 8 caracters", Toast.LENGTH_LONG).show()
+                                        } else {
+                                            Toast.makeText(this@Register, "Erreur inconnu", Toast.LENGTH_LONG).show()
+                                        }
+                                    }
+                                }
+                                500 -> {
+                                    Toast.makeText(this@Register, "L'utilisateur exist déjà", Toast.LENGTH_LONG).show()
+                                    }
+                            }
+                        }
+
+
+                    })
+
+                    ///
+                }
             }
         }
 
@@ -68,12 +104,12 @@ class Register : AppCompatActivity() {
     }
 
     fun signUp(name: String, mail: String, password: String){
-        println(":::::::::: TEST ::::::::::")
+        println(":::::::::: SIGN UP ::::::::::")
 
         var retrofit = RequestUtils.retrofitBase.create(UserInterface::class.java)
         val user = UserRegisterClassItem(email = mail, username = name, password = password, role = "User")
-        println("BREFORE !!!!!!!!!!")
         var retrofitData = retrofit.register(user)
+
         retrofitData.enqueue(object: Callback<UserClassItem>{
             override fun onFailure(call: Call<UserClassItem>, t: Throwable) {
                 Log.d("RegisterActivity", "onfailure: "+ t.message )
@@ -81,12 +117,11 @@ class Register : AppCompatActivity() {
 
             override fun onResponse(call: Call<UserClassItem>, response: Response<UserClassItem>) {
                 val errorStr = response.errorBody()?.string()
-                println("REPONSE ----------> ${response.errorBody()?.string()} === $errorStr" )
 
-                println(response.code())
                 when (response.code()){
                     400 -> {
-                        if (errorStr != null) {
+                        println(errorMessage.isEmpty())
+                        if (errorStr != null && errorMessage.isEmpty()) {
                             errorMessage = if (errorStr.contains("email must be an email")) {
                                 "email doit être un email"
                             } else if (errorStr.contains("password must be longer than or equal to 8 characters")){
@@ -97,8 +132,9 @@ class Register : AppCompatActivity() {
                         }
                     }
                     500 -> {
-                        errorMessage = "L'utilisateur exist déjà"
-                        println(errorMessage)
+                        if (errorMessage.isEmpty()){
+                            errorMessage = "L'utilisateur exist déjà"
+                        }
                     }
                     201 -> SessionManager.logIn(mail, password);
                 }
